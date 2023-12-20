38439a9e8db7f9ca7372ce02e85e7403389467c6
Islam <islambassiem@gmail.com>
2023-12-20 15:07:10 +0300

    start on the dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+use App\Models\Vacation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Helpers\VacationHelperController;
 
 class DashboardController extends Controller
 {
@@ -14,6 +18,103 @@ class DashboardController extends Controller
 
   public function index()
   {
-    return view('dashboard');
+    return view('dashboard', [
+      'availdMonth' => $this->availdVacationThisMonth(),
+      'availdYear' => $this->availdVacationThisYear(),
+      'balance' => $this->balance()
+    ]);
+  }
+
+
+  private function balance($endDate = null)
+  {
+    return round($this->accrued($endDate) - $this->availed(), 0);
+  }
+
+
+  private function accrued($endDate = null)
+  {
+    $endDate ?? date('Y-m-d');
+    $start = Carbon::parse(auth()->user()->joining_date);
+    $end = Carbon::parse($endDate);
+    $days = $this->days($start, $end);
+    $accrued = 0;
+    if(auth()->user()->vacation_class == 30){
+      $accrued =  $days * 30 / 365 ;
+    }
+    if(auth()->user()->vacation_class == 21){
+      $joiningDate = Carbon::parse(auth()->user()->joining_date);
+      $tillDate = Carbon::parse((Carbon::parse($endDate))->addDay());
+      $diff = ($tillDate->diffInDays($joiningDate)) / 365;
+      if($diff >= 5){
+        $accrued = (5 * 21) + (($diff - 5) * 30);
+      }
+      if($diff < 5){
+        $accrued = $days * 21 / 365;
+      }
+    }
+    return $accrued;
+  }
+
+  private function availed()
+  {
+    $availed =  DB::select("SELECT
+      SUM(DATEDIFF(
+        vacations.end_date,
+        vacations.start_date
+      ) +1) AS days
+    FROM vacations WHERE vacation_type = 1 AND `status_id` = 1 AND user_id = ? AND deleted_at IS NULL LIMIT 1;", [auth()->user()->id]);
+    return $availed[0]->days;
+  }
+
+  private function days($start_date, $end_date)
+  {
+    $start = Carbon::parse($start_date);
+    $end = Carbon::parse($end_date);
+    return $end->diffInDays($start) + 1;
+  }
+
+  private function availdVacationThisMonth()
+  {
+    $start = Carbon::now()->startOfMonth()->format('Y-m-d');
+    $end = Carbon::now()->endOfMonth()->format('Y-m-d');
+    return $this->availdInDuration($start, $end);
+  }
+
+  private function availdVacationThisYear()
+  {
+    $start = Carbon::now()->startOfYear()->format('Y-m-d');
+    $end = Carbon::now()->endOfYear()->format('Y-m-d');
+    return $this->availdInDuration($start, $end);
+  }
+
+  public function availdInDuration($start, $end)
+  {
+    $vacations = Vacation::where('user_id', auth()->user()->id)
+    ->where('status_id', '1')
+    ->where('start_date', '<=', $end)
+    ->where('end_date', '>=', $start)
+    ->get();
+    $total = 0;
+    foreach ($vacations as $vacation) {
+      if($vacation->start_date >= $start){
+        $start_date = $vacation->start_date;
+        if($vacation->end_date >= $end){
+          $end_date = $end;
+        }else{
+          $end_date = $vacation->end_date;
+        }
+      }else{
+        $start_date = $start;
+        if($vacation->end_date <= $end){
+          $end_date = $vacation->end_date;
+        }else{
+          $end_date = $end;
+        }
+      }
+      $diff = Carbon::parse($end_date)->diffIndays(Carbon::parse($start_date)) + 1;
+      $total += $diff;
+    }
+    return $total ;
   }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,14 @@
 @endsection
 
 @section('style')
+  <style>
+    .icon{
+      font-size:30px;
+    }
+    .number{
+      font-size: 80px
+    }
+  </style>
 @endsection
 
 @section('h1')
@@ -16,6 +24,130 @@
 @endsection
 
 @section('content')
+
+  <div class="row">
+    <div class="col-md-8">
+      <div class="row">
+        <div class="col-xxl-4 col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title text-center">
+                {{ __('Vacations Availd') }}
+              </h5>
+              <div class="text-center text-info icon">
+                <i class="bi bi-calendar3-week-fill"></i>
+              </div>
+              <div class="number text-center">{{ $availdMonth }}</div>
+              <div class="card-title text-center">{{ __('This Month') }}</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xxl-4 col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title text-center">
+                {{ __('Vacations Availd') }}
+              </h5>
+              <div class="text-center text-danger icon">
+                <i class="bi bi-bank"></i>
+              </div>
+              <div class="number text-center">{{ $availdYear }}</div>
+              <div class="card-title text-center">{{ __('This Year') }}</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xxl-4 col-xl-12">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title text-center">
+                {{ __('Vacation Balance') }}
+              </h5>
+              <div class="text-center text-success icon">
+                <i class="bi bi-graph-down"></i>
+              </div>
+              <div class="number text-center">{{ $balance }}</div>
+              <div class="card-title text-center">{{ __('APL') }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    {{-- <div class="col-lg-4">
+      <!-- Recent Activity -->
+      <div class="card">
+        <div class="filter">
+          <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+            <li class="dropdown-header text-start">
+              <h6>Filter</h6>
+            </li>
+
+            <li><a class="dropdown-item" href="#">Today</a></li>
+            <li><a class="dropdown-item" href="#">This Month</a></li>
+            <li><a class="dropdown-item" href="#">This Year</a></li>
+          </ul>
+        </div>
+
+        <div class="card-body">
+          <h5 class="card-title">Recent Activity <span>| Today</span></h5>
+
+          <div class="activity">
+
+            <div class="activity-item d-flex">
+              <div class="activite-label">32 min</div>
+              <i class="bi bi-circle-fill activity-badge text-success align-self-start"></i>
+              <div class="activity-content">
+                Quia quae rerum <a href="#" class="fw-bold text-dark">explicabo officiis</a> beatae
+              </div>
+            </div><!-- End activity item-->
+
+            <div class="activity-item d-flex">
+              <div class="activite-label">56 min</div>
+              <i class="bi bi-circle-fill activity-badge text-danger align-self-start"></i>
+              <div class="activity-content">
+                Voluptatem blanditiis blanditiis eveniet
+              </div>
+            </div><!-- End activity item-->
+
+            <div class="activity-item d-flex">
+              <div class="activite-label">2 hrs</div>
+              <i class="bi bi-circle-fill activity-badge text-primary align-self-start"></i>
+              <div class="activity-content">
+                Voluptates corrupti molestias voluptatem
+              </div>
+            </div><!-- End activity item-->
+
+            <div class="activity-item d-flex">
+              <div class="activite-label">1 day</div>
+              <i class="bi bi-circle-fill activity-badge text-info align-self-start"></i>
+              <div class="activity-content">
+                Tempore autem saepe <a href="#" class="fw-bold text-dark">occaecati voluptatem</a> tempore
+              </div>
+            </div><!-- End activity item-->
+
+            <div class="activity-item d-flex">
+              <div class="activite-label">2 days</div>
+              <i class="bi bi-circle-fill activity-badge text-warning align-self-start"></i>
+              <div class="activity-content">
+                Est sit eum reiciendis exercitationem
+              </div>
+            </div><!-- End activity item-->
+
+            <div class="activity-item d-flex">
+              <div class="activite-label">4 weeks</div>
+              <i class="bi bi-circle-fill activity-badge text-muted align-self-start"></i>
+              <div class="activity-content">
+                Dicta dolorem harum nulla eius. Ut quidem quidem sit quas
+              </div>
+            </div><!-- End activity item-->
+
+          </div>
+
+        </div>
+      </div><!-- End Recent Activity -->
+    </div> --}}
+  </div>
+
 @endsection
 
 @section('script')
