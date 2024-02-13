419e8b747d81a8c1fdfd6afe1c1481039f10401a
Islam <islambassiem@gmail.com>
2024-02-13 12:38:23 +0300

    adding Chart to HR Dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
   public function index()
   {
-    return view('admin.dashboard');
+    $numbers = DB::select(
+      'select 
+      _categories.category_en
+        , _categories.category_ar
+        , users.category_id
+        , count(users.id) as user_count
+    from users 
+    join _categories on _categories.id = users.category_id
+    where active = ?
+    group by users.category_id, _categories.category_en, _categories.category_ar', [1]);
+    $results[] = ['category_id', 'user_count'];
+    foreach ($numbers as $key => $value) {
+      $results[++$key] = [(string) $value->{'category' . session('_lang')}, (int) $value->user_count];
+    }
+    return view('admin.dashboard', [
+      'results' => $results
+    ]);
   }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,7 +15,39 @@
 @endsection
 
 @section('content')
+  <section class="section">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">
+          Employees Numbers
+        </h5>
+        <div id="piechart_3d" style="width: 100%; height: 500px;"></div>
+      </div>
+    </div>
+  </section>
 @endsection
 
 @section('script')
+  <script src="{{ asset('assets/js/loader.js') }}"></script>
+  <script type="text/javascript">
+    google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable({{ Js::from($results) }});
+      var options = {
+        title: 'My Daily Activities',
+        is3D: true,
+      }
+      var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+      google.visualization.events.addListener(chart, 'select', function () {
+        var selectedItem = chart.getSelection()[0];
+        if (selectedItem) {
+          // Redirect to another page with query parameters
+          var selectedValue = data.getValue(selectedItem.row, 0); // Assuming you have a single column in your DataTable
+          window.location.href = 'your_destination_page.php?param=' + selectedValue;
+        }
+      });
+      chart.draw(data, options);
+    }
+  </script>
 @endsection
