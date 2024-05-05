30c91282341c2b494143bd5f7362620e3fb6df76
Islam <islambassiem@gmail.com>
2024-05-05 10:56:40 +0300

    checking if a amonth is open, otherwise open a new one

---
+++ app/Http/Controllers/Admin/Salaries/OpenMonth.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Salaries;
+
+use App\Models\Admin\Month;
+use Carbon\Carbon;
+
+trait openMonth
+{
+  public function openMonth()
+  {
+    if(Month::where('status', '0')->first() == null)
+    {
+      $last_month = Month::where('status', '1')->orderByDesc('month')->first();
+      $start_date = Carbon::parse($last_month->end_date)->addDay();
+      $end_date = Carbon::parse($last_month->end_date)->copy()->addMonth();
+      $month = $last_month->month == 12 ? 1 :  $last_month->month + 1;
+      $year = $last_month->month == 12 ? $last_month->year + 1 : $last_month->year;
+      Month::create([
+        'start_date' => $start_date,
+        'end_date' => $end_date,
+        'month' => $month,
+        'year' => $year,
+        'user_id' => 71
+      ]);
+    }
+    return Month::orderByDesc('created_at')->first();
+  }
+}
