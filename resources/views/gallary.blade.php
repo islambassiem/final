e77ed24a9560854200ec5ba9fc21569ce1cd306c
Islam Mac <islam@mac.com>
2025-07-29 14:34:58 +0300

    adding vision2030 posts insread of dashoard

--- resources/views/gallary.blade.php
+++ resources/views/gallary.blade.php
@@ -12,22 +12,22 @@
     <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="{{ asset('storage/gallary/0.webp') }}" class="d-block w-100" alt="...">
+                <img src="{{ asset('storage/gallary/0.webp') }}" class="d-block w-100" alt="0">
             </div>
             <div class="carousel-item">
-                <img src="{{ asset('storage/gallary/1.webp') }}" class="d-block w-100" alt="...">
+                <img src="{{ asset('storage/gallary/1.webp') }}" class="d-block w-100" alt="1">
             </div>
             <div class="carousel-item">
-                <img src="{{ asset('storage/gallary/2.webp') }}" class="d-block w-100" alt="...">
+                <img src="{{ asset('storage/gallary/2.webp') }}" class="d-block w-100" alt="2">
             </div>
             <div class="carousel-item">
-                <img src="{{ asset('storage/gallary/3.webp') }}" class="d-block w-100" alt="...">
+                <img src="{{ asset('storage/gallary/3.webp') }}" class="d-block w-100" alt="3">
             </div>
             <div class="carousel-item">
-                <img src="{{ asset('storage/gallary/4.webp') }}" class="d-block w-100" alt="...">
+                <img src="{{ asset('storage/gallary/4.webp') }}" class="d-block w-100" alt="4">
             </div>
             <div class="carousel-item">
-                <img src="{{ asset('storage/gallary/5.webp') }}" class="d-block w-100" alt="...">
+                <img src="{{ asset('storage/gallary/5.webp') }}" class="d-block w-100" alt="5">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
